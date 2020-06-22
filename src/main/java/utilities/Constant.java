5da4c40eb7316888af7e4924d84a36d769aa2f2a
Kajal Gaikwad <66130116+kdgaikwad@users.noreply.github.com>
2020-06-22 12:01:26 +0530

    Update Constant.java

--- src/main/java/utilities/Constant.java
+++ src/main/java/utilities/Constant.java
@@ -3,9 +3,9 @@ package utilities;
 public class Constant {
 	 public static final String URL = "https://www.biba.com/";
 	 
-     public static final String Username = "kdgaikwad020@gmail.com";
+     public static final String Username = "dummy.com";
 
-     public static final String Password = "Maya#220699";
+     public static final String Password = "1234";
 
      public static final String Path_TestData = "C:\\Users\\Sarvadnya\\eclipse-workspace\\shoppingwebsite\\src\\test\\resources\\";
      public static final String File_TestData = "TestData.xlsx";
